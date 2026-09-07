19a9bc04a9ed78a6218a0c6a7a22f61586f0a6ab
Enamul <enamul@outlook.com>
2026-09-06 22:27:42 -0500

    changed from = to :

--- scratchpad.py
+++ scratchpad.py
@@ -1,6 +1,6 @@
 #variables and arithmatic operators
 name,age,height,birthYear = "Tom",29,5.5,1999
-print(f"name={name}")
-print(f"age={age}")
-print(f"height={height} ft")
-print(f"birthYear={birthYear}")
+print(f"name: {name}")
+print(f"age: {age}")
+print(f"height: {height} ft")
+print(f"birthYear: {birthYear}")
